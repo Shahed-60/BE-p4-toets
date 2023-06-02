945e82e7cfcb3cd884b4aa093e031907038c3bfd
Shahed Amer <chanyeol6521354@gmail.com>
2023-06-02 12:37:18 +0200

    dingen verwijdert

--- app/models/ExaminatornModel.php
+++ app/models/ExaminatornModel.php
@@ -20,34 +20,7 @@ class InstructeurModel
         return $this->db->resultSet();
     }
 
-    // public function getGegevensInstructeur()
-    // {
-    //     $sql = "SELECT ty.typeVoertuig, vo.Type, vo.Kenteken, vo.Bouwjaar, vo.Brandstof, ty.RijbewijsCategorie
-    //             from instructeur ins
-    //             left join voertuiginstructeur voins
-    //             on ins.Id = voins.Id
-    //             left join voertuig vo
-    //             on vo.Id = ins.Id
-    //             left join typevoertuig ty
-    //             on ty.Id = vo.typevoertuigId
-    //             where ins.Id = 1
-    //             order by ty.rijbewijscategorie desc";
 
-    //     $this->db->query($sql);
-
-    //     return $this->db->resultSet();
-    // }
-
-    // public function getLizhan()
-    // {
-    //     $sql = "SELECT * 
-    //     FROM instructeur
-    //     where Id = 1";
-
-    //     $this->db->query($sql);
-
-    //     return $this->db->resultSet();
-    // }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // MENEER WERK
     public function getInstructeurInfoById($instructeurId)
